b772406f665f4933e0ce7de3721acbae7cf51c43
edusocrates <edu5390@gmail.com>
2023-08-23 16:25:38 -0300

    Initial commit

---
+++ src/test/java/br/com/estudos/edusocrates/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.edusocrates.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
